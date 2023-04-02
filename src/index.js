d3afa5977f6002b67c264fb74592c51b09466442
pobre <nacholapto@gmail.com>
2023-04-02 22:08:36 +0200

    feat: subiendo cambios

--- src/index.js
+++ src/index.js
@@ -1,30 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { TRANSITIONS } from './utils/TRANSITIONS'
-import routerEvents from 'next-router-events'
 
 const NextjsTransition = ({ 
     children, 
-    router,
     animation = "default",
     duration,
     isCustom = false,
     customAnimation,
 }) => {
-
-    const [routerApi, setRouterApi] = useState('/')
-
-    const logUrl = url => {
-        console.log("url change", url)
-        setRouterApi(url)
-    }
-
-    routerEvents.on('routeChangeComplete', logUrl)
     
     return (
         <AnimatePresence mode="wait" >
             <motion.div
-                key={routerApi}
                 initial="initialState"
                 animate="animateState"
                 exit="exitState"
